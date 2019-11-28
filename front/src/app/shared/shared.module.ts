c8f803dbddc9f07926117038f02e7b53b38b4fdd
Steve Sossa <esteban.ocampo.cba@gmail.com>
2019-11-28 16:17:23 -0300

    FE_footer-deleted

--- front/src/app/shared/shared.module.ts
+++ front/src/app/shared/shared.module.ts
@@ -1,19 +1,15 @@
 import { NgModule } from '@angular/core';
 import { NotAllowedComponent } from './not-allowed/not-allowed.component';
-import { FooterComponent } from './footer/footer.component';
 import { EmptyStateComponent } from './empty-state/empty-state.component';
-import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   imports: [ ],
   exports: [
     NotAllowedComponent,
-    FooterComponent,
     EmptyStateComponent
   ],
   declarations: [
     NotAllowedComponent,
-    FooterComponent,
     EmptyStateComponent
   ],
   entryComponents: [ ],
